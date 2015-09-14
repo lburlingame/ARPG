caca149ae4444553c59c3c82eef2df7c3c4e7e67
lburlingame <lelandburlingame@gmail.com>
2015-09-14 13:56:46 -0700

    Fixed skin dispose

--- Game2/core/src/com/haruham/game/state/Play.java
+++ Game2/core/src/com/haruham/game/state/Play.java
@@ -56,13 +56,13 @@ public class Play extends GameState {
     private World world;
     private Body playerBody;
     private PlayerContactListener contact;
-    private Sound wavSound = Gdx.audio.newSound(Gdx.files.internal("audio/sfx/spooky1.wav"));
+    private Sound wavSound = Gdx.audio.newSound(Gdx.files.internal("audio/sfx/pickup1.wav"));
 
     private GameMenuInput gin;
 
     public Play(GameStateManager gsm) {
         super(gsm);
-        //wavSound.loop(.4f, 2f,.1f);
+        //wavSound.loop(.4f, 1f,.1f);
 
         gin = new GameMenuInput(gsm);
 
